e8a426052c6a96d85251c896b5c9f9bf623c6b47
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2018-12-24 22:36:18 +0300

    Do my first homework

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+Artist = "Oxxxymiron" #artist name
+Genre = "Rap or Hip Hop" #genre name
+Song = "Город под подошвой" #song name on Russian
+DurationInSeconds = str(4*60+7) + " seconds" #song duration
+Year = 2015 # year 
+ViewsOnYoutube = 47000000 #views count
+PublicationDate = "21.09.2015" #publication date on youtube
+MyPersonalScore = str(4.5) + " out of " + str(5) #my personal score rating of this song
+
+
+
+print(Artist)
+print(Genre)
+print(Song)
+print(DurationInSeconds)
+print(Year)
+print(ViewsOnYoutube)
+print(PublicationDate)
+print(MyPersonalScore)
