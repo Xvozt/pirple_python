c332965230ead47f152f8759804976e8650e7618
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2018-12-26 02:30:30 +0300

    PEP8 corretions

--- functions.py
+++ functions.py
@@ -1,38 +1,42 @@
 # function for artist
 def artistFunc():
-	Name = "Oxxxymiron"
-	return Name
+    Name = "Oxxxymiron"
+    return Name
+
 
 print(artistFunc())
 
 
 # function for Gengre
 def genreFunc():
-	Name = "Rap or Hip-Hop"
-	return Name
+    Name = "Rap or Hip-Hop"
+    return Name
+
 
 print(genreFunc())
 
 
 # function for Year
 def yearFunc():
-	Name = 2015
-	return Name
+    Name = 2015
+    return Name
+
 
 print(yearFunc())
-
-
-#True/False function
 TooLow = 50000
 TooHigh = 50000000
+
+
+# True/False function
 def viewsFunc(number):
-	if number < 47000000:
-		return True
-	else:
-		return False
+
+    if number < 47000000:
+        return True
+    else:
+        return False
+
+
 A = viewsFunc(TooHigh)
 B = viewsFunc(TooLow)
 print(A)
 print(B)
-
-
