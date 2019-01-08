6a12268ff0c17b6450a2ec4e325d9615f6dc2db9
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-01-08 18:26:12 +0300

    Do homework #5

---
+++ bizz_fuzz_game.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# BizzFuzz game
+
+
+def isPrime(number):
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    i = 2
+    limit = int(number ** 0.5)
+    while i <= limit:
+        if number % i == 0:
+            return False
+        i += 1
+    return True
+
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif isPrime(i) is True:
+        print("Prime")
+    else:
+        print(i)
