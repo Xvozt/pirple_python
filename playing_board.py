a6e461540e0b5b6e54580a6e7c060a4fc24c96dc
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-01-13 18:43:35 +0300

    Move width and height checker

--- playing_board.py
+++ playing_board.py
@@ -13,23 +13,23 @@ printing the above shapes with specified number of rows and columns
 """
 
 def draw_board(rows, columns):  
-    if columns <= 72 and rows <= 13:
-        verticals = columns*2 # real number of elements that define the column
-        horizontals = rows*2 # real number of elements that define the row
-        for row in range(horizontals-1):
-            if row%2 == 0:
-                for column in range(1, verticals):
-                    if column % 2 == 1:
-                        if column != verticals-1:
-                            print(" ", end = "")
-                        else:
-                            print(" ")
+    verticals = columns*2 # real number of elements that define the column
+    horizontals = rows*2 # real number of elements that define the row
+    for row in range(horizontals-1):
+        if row%2 == 0:
+            for column in range(1, verticals):
+                if column % 2 == 1:
+                    if column != verticals-1:
+                        print(" ", end = "")
                     else:
-                        print("|", end = "")
-            else:
-                print("-"*(verticals-1))
+                        print(" ")
+                else:
+                    print("|", end = "")
+        else:
+            print("-"*(verticals-1))
+    if columns <= 72 and rows <= 13:
         return True
     else:
         return False
 
-print(draw_board(5, 72))
+print(draw_board(5, 70))
