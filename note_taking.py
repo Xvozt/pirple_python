97fa339e692f91934f709f274d3da3380fdf5bc0
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-02-05 01:20:00 +0300

    new option to the note_type function

--- note_taking.py
+++ note_taking.py
@@ -1,20 +1,23 @@
 import os
 
 question = input("Enter the filename: ")
-filename = question+'.txt'
+filename = question + '.txt'
+
 
 def note_type(name):
-    if os.path.isfile('./'+name+'.txt') == True:
-        return input('input A if u wanna read the file \n' + 
-        'Input B if u wanna delete the file and start over \n' + 
-        'Input C if u wanna append the file')
+    if os.path.isfile('./' + name + '.txt') == True:
+        return input('input A if u wanna read the file \n' +
+                     'Input B if u wanna delete the file and start over \n' +
+                     'Input C if u wanna append the file \n' + 
+                     'Input D if u wanna replace a single line \n')
     else:
-        return input('Input the text you wanna write to the file')
+        return input('Input the text you wanna write to the file: ')
 
 
 def writer():
     text = str(note_type(question))
     with open(filename, 'w') as f:
         f.write(text)
+
 
 writer()
